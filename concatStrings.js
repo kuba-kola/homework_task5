63a1c1ef0fa7e4ebe31e2bf42d63bd655976fde9
Yakau Trafimovich <5243212@gmail.com>
2022-03-21 21:43:38 +0100

    delete console.log

--- concatStrings.js
+++ concatStrings.js
@@ -3,7 +3,7 @@ function concatStrings(input, separator = '') {
         const res = input + separator + args;
         const argsCorrect = typeof args === 'string';
         const sepCorrect = typeof separator === 'string';
-        
+
         if (!sepCorrect) {
             separator = '';
         }
@@ -11,21 +11,7 @@ function concatStrings(input, separator = '') {
         if (!argsCorrect) {
             return input;
         }         
-  
+
         return concatStrings(res, separator);
     }
 }
-
-console.log(concatStrings('first')('second')('third')());
-// 'firstsecondthird'
-console.log(concatStrings('first', null)('second')())
-//'firstsecond'
-console.log(concatStrings('first', '123')('second')('third')())
-//'first123second123third'
-console.log(concatStrings('some-value')('')('')(null))
-//'some-value'
-console.log(concatStrings('some-value')(2))
-//'some-value'
-console.log(concatStrings('some-value')('333')(123n))
-//'some-val333'
-
