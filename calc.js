d0c7514c33bb920465ed783941f255e70c166acd
Yakau Trafimovich <5243212@gmail.com>
2022-03-23 15:46:05 +0100

    add arrow function

--- calc.js
+++ calc.js
@@ -1,21 +1,20 @@
 class Calculator {
   constructor(x, y) {    
-    this.x = x;
-    this.y = y;
-    this.setX = this.setX.bind(this);
-    this.setY = this.setY.bind(this);
-    this.logSum = this.logSum.bind(this);
-    this.logMul = this.logMul.bind(this);
-    this.logSub = this.logSub.bind(this);
-    this.logDiv = this.logDiv.bind(this);
-        
     if (!Number.isFinite(x) || !Number.isFinite(y)) {
       throw new Error(`Ошибка! Вы ввели не число!`);
-
     }
+
+    this.x = x;
+    this.y = y;
+    // this.setX = this.setX.bind(this);
+    // this.setY = this.setY.bind(this);
+    // this.logSum = this.logSum.bind(this);
+    // this.logMul = this.logMul.bind(this);
+    // this.logSub = this.logSub.bind(this);
+    // this.logDiv = this.logDiv.bind(this);        
   }
 
-  setX(num) {
+  setX = num => {
     if (!Number.isFinite(num)) {
       throw new Error(`Ошибка! Вы ввели не число!`);
     }
@@ -23,7 +22,7 @@ class Calculator {
     this.x = num;
   }
 
-  setY(num) {
+  setY = num => {
     if (!Number.isFinite(num) || num === 0) {
       throw new Error(`Ошибка! Вы ввели не число!`);
     }
@@ -31,19 +30,19 @@ class Calculator {
     this.y = num;
   }
 
-  logSum() {
+  logSum = () => {
     console.log(this.x + this.y);
   }
 
-  logMul() {
+  logMul = () => {
     console.log(this.x * this.y);
   }
 
-  logSub() {
+  logSub = () => {
     console.log(this.x - this.y);
   }
 
-  logDiv() {
+  logDiv = () => {
     if (this.y === 0) {
       throw new Error(`Ошибка! Вы пытаесь поделить на ноль!`);
     }
@@ -60,4 +59,4 @@ calculator.logDiv(); // 5
 const logCalculatorDiv = calculator.logDiv;
 logCalculatorDiv(); // 5
 calculator.setX('tt'); // Ошибка!
-calculator.setY('td78'); // Ошибка!
+// calculator.setY('td78'); // Ошибка!
